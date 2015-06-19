bde09f1c63eb7a08140fc7d88e0ec5dfe7e4de39
Explicitname <explicit6911@gmail.com>
2015-06-19 14:19:43 -0400

    Second Exercise Finished

--- main.cpp
+++ main.cpp
@@ -4,20 +4,16 @@
 
 using namespace std;
 
-string Hex(void* p) {
-
+string Hex(void* p)
+{
 stringstream stream;
-
 stream << hex << setfill('0') << setw(8) << (long) p << ends;
-
 return stream.str();
-
 }
 
-void Exercise1() {
-
+void Exercise1()
+{
 cout << "Exercise 1" << endl << endl;
-stringstream outPut;
 int x = 2;
 int y = 8;
 int* p = &x;
@@ -34,25 +30,51 @@ cout << "The Value of X is: " << x << endl
 		<< "The Value of Q is: " << Hex(q) << endl
 		<< "The Value of *Q is: " << *q << endl;
 
-
 cin.ignore();
 cout << "Exercise 1 Complete" << endl;
-
 }
 
+void Exercise2()
+{
+cout << "Exercise 2" << endl << endl;
+int x = 3;
+int y = 4;
+int z = 5;
+int* p = &x;
+int* q = &y;
+int* r = &z;
+
+cout << "The Value of X is: " << x << endl
+		<< "The Value of Y is: " << y << endl
+		<< "The Value of Z is: " << z << endl
+		<< "The Value of P is: " << Hex(p) << endl
+		<< "The Value of *P is: " << *p << endl
+		<< "The Value of Q is: " << Hex(q) << endl
+		<< "The Value of *Q is: " << *q << endl
+		<< "The Value of R is: " << Hex(r) << endl
+		<< "The Value of *R is: " << *r << endl << endl
+		<< "Swapping Values..." << endl << endl;
+
+z = x; x = y; y = z;
+
+cout << "The Value of X is: " << x << endl
+		<< "The Value of Y is: " << y << endl
+		<< "The Value of Z is: " << z << endl
+		<< "The Value of P is: " << Hex(p) << endl
+		<< "The Value of *P is: " << *p << endl
+		<< "The Value of Q is: " << Hex(q) << endl
+		<< "The Value of *Q is: " << *q << endl
+		<< "The Value of R is: " << Hex(r) << endl
+		<< "The Value of *R is: " << *r << endl << endl;
+
+cin.ignore();
+cout << "Exercise 2 Complete" << endl;
+}
 
 int main()
 {
-
 Exercise1();
+Exercise2();
 
-
-
-
-
-
-
-
-
-	return 0;
+return 0;
 }
