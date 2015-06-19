02c441a1f8a865dcdce9119f1c98bc68c839beda
Explicitname <explicit6911@gmail.com>
2015-06-19 14:41:07 -0400

    Third and Fourth Exercises Finished

--- main.cpp
+++ main.cpp
@@ -71,10 +71,71 @@ cin.ignore();
 cout << "Exercise 2 Complete" << endl;
 }
 
+void Exercise3()
+{
+cout << "Exercise 3" << endl << endl;
+int x = 3;
+int y = 4;
+int z = 5;
+int* p = &x;
+int* q = &y;
+int* r = &z;
+
+cout << "The Value of X is: " << x << endl
+		<< "The Value of Y is: " << y << endl
+		<< "The Value of Z is: " << z << endl
+		<< "The Value of P is: " << Hex(p) << endl
+		<< "The Value of *P is: " << *p << endl
+		<< "The Value of Q is: " << Hex(q) << endl
+		<< "The Value of *Q is: " << *q << endl
+		<< "The Value of R is: " << Hex(r) << endl
+		<< "The Value of *R is: " << *r << endl << endl
+		<< "Swapping Values..." << endl << endl;
+
+r = p; p = q; q = r;
+
+cout << "The Value of X is: " << x << endl
+		<< "The Value of Y is: " << y << endl
+		<< "The Value of Z is: " << z << endl
+		<< "The Value of P is: " << Hex(p) << endl
+		<< "The Value of *P is: " << *p << endl
+		<< "The Value of Q is: " << Hex(q) << endl
+		<< "The Value of *Q is: " << *q << endl
+		<< "The Value of R is: " << Hex(r) << endl
+		<< "The Value of *R is: " << *r << endl << endl;
+
+cin.ignore();
+cout << "Exercise 3 Complete" << endl;
+}
+
+void Exercise4()
+{
+cout << "Exercise 4" << endl << endl;
+int x = 11;
+int y = 12;
+int a[5];
+int u = 31;
+int v = 32;
+
+for (int i =0;i < 5;i++)
+{
+	int tempNum = 21;
+	a[i] = tempNum + i;
+	cout << a[i] << endl;
+}
+cout << endl;
+for (int i = -2; i < 7; i++)
+{
+cout << setw(2) << i << " " << setw(2) << a[i] << endl;
+}
+}
+
 int main()
 {
-Exercise1();
-Exercise2();
+//Exercise1();
+//Exercise2();
+//Exercise3();
+Exercise4();
 
 return 0;
 }
